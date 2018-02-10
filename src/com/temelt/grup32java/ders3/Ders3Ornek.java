cd34476dc8bbc7b17047ce6e450b9fc7bc7adac6
temelt <tanertemel@outlook.com>
2018-02-10 11:57:02 +0200

    Tekrar Gönderim

--- src/com/temelt/grup32java/ders3/Ders3Ornek.java
+++ src/com/temelt/grup32java/ders3/Ders3Ornek.java
@@ -2,7 +2,12 @@ package com.temelt.grup32java.ders3;
 
 public class Ders3Ornek {
 
+    /**
+     * Github gönderimi için
+     */
     public void gitOrnek() {
         System.out.println("Git örneği");
+
+
     }
 }

38a43fc2b6e9c72a17f9d58c0ed1131bfccecf9e
temelt <tanertemel@outlook.com>
2018-02-10 11:40:03 +0200

    init

---
+++ src/com/temelt/grup32java/Uygulama.java
@@ -0,0 +1,22 @@
+package com.temelt.grup32java;
+
+import com.temelt.grup32java.ders3.Ders3;
+
+import java.util.Scanner;
+
+public class Uygulama {
+
+    public static void main(String[] args) {
+
+
+        Ders3 ders3Nesnesi =new Ders3();
+        ders3Nesnesi.ornekMetod();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Yaşınızı Giriniz:");
+        int t = scanner.nextInt();
+        ders3Nesnesi.parametreliMethod(t);
+
+    }
+
+}
---
+++ src/com/temelt/grup32java/ders3/Ders3.java
@@ -0,0 +1,14 @@
+package com.temelt.grup32java.ders3;
+
+public class Ders3 {
+
+    public void ornekMetod() {
+        System.out.println("Örnek Metod Çalıştı");
+    }
+
+    public void parametreliMethod(int yas) {
+        int mevcutYil = 2018;
+        int dogumYili = mevcutYil - yas;
+        System.out.println("Doğum Yiliniz :" + dogumYili);
+    }
+}
